df75833a02b6a8dd93d6fbe40fa76c374454dd9c
Mahmud Hasan Jion <mahmudh.jion02@gmail.com>
2025-02-22 06:57:05 +0000

    Add new plugins and ESLint module to nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/my-plugin.js',
+    '~/plugins/new-plugin.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -29,6 +31,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/eslint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
